5a783ac78aa95963597bff86f3effb9e7142a49f
su <admin>
2020-12-01 22:42:40 +0200

    add sign in

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from accounts import views
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    path('profile/', views.user_profile, name='carrent_user_profile'),
+    path('profile/', views.user_profile, name='current_user_profile'),
     path('profile/<int:pk>/', views.user_profile, name='user_profile'),
     path('signup/', views.signup_user, name='signup_user'),
 ]
